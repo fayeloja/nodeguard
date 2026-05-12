fa115f4b6d0baaae07421cc9dc7dbd6c5611af9c
Fatai Ayeloja <fayeloja@gmail.com>
2026-05-12 17:56:25 +0100

    updated llm to switch when openai is not available

--- graph/llm.py
+++ graph/llm.py
@@ -5,9 +5,17 @@ import os
 load_dotenv()
 
 def get_llm():
-    return ChatOpenAI(
-        model="llama-3.3-70b-versatile",
-        openai_api_key=os.getenv("GROQ_API_KEY"),
-        openai_api_base="https://api.groq.com/openai/v1",
-        temperature=0.2,
-    )
+    openai_key = os.getenv("OPENAI_API_KEY")
+    
+    if openai_key:
+        return ChatOpenAI(
+            model="gpt-4o",
+            temperature=0.2,
+        )
+    else:
+        return ChatOpenAI(
+            model="llama-3.3-70b-versatile",
+            openai_api_key=os.getenv("GROQ_API_KEY"),
+            openai_api_base="https://api.groq.com/openai/v1",
+            temperature=0.2,
+        )
